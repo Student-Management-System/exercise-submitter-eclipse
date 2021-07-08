f09d3a8f560d9a566dd815a1a14c6f9c19d694d9
Adam <adam@sse.uni-hildesheim.de>
2021-07-08 14:42:48 +0200

    Add CheckSubmission action

---
+++ src/net/ssehub/teaching/exercise_submitter/eclipse/actions/CheckSubmission.java
@@ -0,0 +1,19 @@
+package net.ssehub.teaching.exercise_submitter.eclipse.actions;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+
+/**
+ * Checks if the selected project matches the submitted version. Shows differences of the two.
+ * 
+ * @author Adam
+ */
+public class CheckSubmission extends AbstractSingleProjectAction {
+
+    @Override
+    protected void execute(IProject project, IWorkbenchWindow window) {
+        MessageDialog.openWarning(window.getShell(), "Exercise Submitter", "Not yet implemented");
+    }
+
+}
