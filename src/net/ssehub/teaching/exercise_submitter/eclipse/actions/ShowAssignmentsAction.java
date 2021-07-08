04c84eb28e63adc0f98cfad36d22c091bb77709d
Adam <adam@sse.uni-hildesheim.de>
2021-07-08 14:43:38 +0200

    Remove unused variable

--- src/net/ssehub/teaching/exercise_submitter/eclipse/actions/ShowAssignmentsAction.java
+++ src/net/ssehub/teaching/exercise_submitter/eclipse/actions/ShowAssignmentsAction.java
@@ -9,11 +9,13 @@ import net.ssehub.teaching.exercise_submitter.eclipse.Activator;
 import net.ssehub.teaching.exercise_submitter.eclipse.dialog.AssignmentDialog;
 
 public class ShowAssignmentsAction extends AbstractHandler {
+
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
         AssignmentDialog assDialog = new AssignmentDialog(new Shell(),
                 Activator.getEclipseManager().getManager().getAllAssignments(), AssignmentDialog.Sorted.GROUPED);
-        int result = assDialog.open();
+        assDialog.open();
         return null;
     }
+
 }
