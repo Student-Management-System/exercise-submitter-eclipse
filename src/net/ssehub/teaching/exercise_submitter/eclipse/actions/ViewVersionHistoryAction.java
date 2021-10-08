bdf6ea68729df0d6234621a4635a54bdffce00a3
Lukas Dönges <lukasdoenges@t-online.de>
2021-10-08 21:57:10 +0200

    fix checkstyle error

--- src/net/ssehub/teaching/exercise_submitter/eclipse/actions/ViewVersionHistoryAction.java
+++ src/net/ssehub/teaching/exercise_submitter/eclipse/actions/ViewVersionHistoryAction.java
@@ -1,16 +1,16 @@
 package net.ssehub.teaching.exercise_submitter.eclipse.actions;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.Version;
 
-import net.ssehub.teaching.exercise_submitter.eclipse.background.ListVersionsJob;
 
+/**
+ * Action for display the version history for the selected Project.
+ * @author lukas
+ *
+ */
 public class ViewVersionHistoryAction extends AbstractSingleProjectAction {
 
     @Override
