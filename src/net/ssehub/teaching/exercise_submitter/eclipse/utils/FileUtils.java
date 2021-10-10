2f9dc85d6e3cba0402080094a68c3ef1f6d6c222
Lukas Dönges <lukasdoenges@t-online.de>
2021-10-10 16:48:53 +0200

    java doc

--- src/net/ssehub/teaching/exercise_submitter/eclipse/utils/FileUtils.java
+++ src/net/ssehub/teaching/exercise_submitter/eclipse/utils/FileUtils.java
@@ -2,13 +2,21 @@ package net.ssehub.teaching.exercise_submitter.eclipse.utils;
 
 import java.io.File;
 
+/**
+ * This class has some FileUtils in it.
+ * @author lukas
+ *
+ */
 public class FileUtils {
-
+    /**
+     * Deletes the contant of a folder.
+     * @param folder
+     */
     public static void deleteContentInFolder(File folder) {
         File[] files = folder.listFiles();
-        if(files!=null) { 
-            for(File f: files) {
-                if(f.isDirectory()) {
+        if (files != null) { 
+            for (File f: files) {
+                if (f.isDirectory()) {
                     deleteContentInFolder(f);
                 } else {
                     f.delete();
