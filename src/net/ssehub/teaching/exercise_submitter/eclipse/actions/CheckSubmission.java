610a4f616f5ea985648fe6dd190513462631161b
Lukas Dönges <lukasdoenges@t-online.de>
2021-10-12 19:25:33 +0200

    added load pref to checksubmission

--- src/net/ssehub/teaching/exercise_submitter/eclipse/actions/CheckSubmission.java
+++ src/net/ssehub/teaching/exercise_submitter/eclipse/actions/CheckSubmission.java
@@ -2,6 +2,7 @@ package net.ssehub.teaching.exercise_submitter.eclipse.actions;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -13,6 +14,7 @@ import net.ssehub.teaching.exercise_submitter.eclipse.dialog.AdvancedExceptionDi
 import net.ssehub.teaching.exercise_submitter.eclipse.dialog.AssignmentDialog;
 import net.ssehub.teaching.exercise_submitter.eclipse.dialog.CheckSubmissionDialog;
 import net.ssehub.teaching.exercise_submitter.eclipse.log.EclipseLog;
+import net.ssehub.teaching.exercise_submitter.eclipse.preferences.ProjectManagerException;
 import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
 import net.ssehub.teaching.exercise_submitter.lib.replay.ReplayException;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.ApiException;
@@ -38,12 +40,17 @@ public class CheckSubmission extends AbstractSingleProjectAction {
         this.project = project;
 
         try {
-            Assignment selectedAssignment = this.displayAssignmentDialog(window);
+            
+            Optional<Assignment> selectedAssignment = chooseAssignment(project, window);
+            
+            if (selectedAssignment.isEmpty()) {
+                throw new ReplayException("No assignment is selected");
+            }
 
             EclipseLog.info("Starting download newest Version ");
 
             CheckSubmissionJob job = new CheckSubmissionJob(window.getShell(),
-                    Activator.getDefault().getManager().getReplayer(selectedAssignment), selectedAssignment,
+                    Activator.getDefault().getManager().getReplayer(selectedAssignment.get()), selectedAssignment.get(),
                     project.getLocation().toFile(), this::onCheckSubmissionFinished);
             job.setUser(true);
             job.schedule();
@@ -60,6 +67,37 @@ public class CheckSubmission extends AbstractSingleProjectAction {
         } catch (IllegalArgumentException | IOException e) {
             AdvancedExceptionDialog.showUnexpectedExceptionDialog(e, "IO Exception");
         }
+    }
+    /**
+     * 
+     * @param project
+     * @param window
+     * @return Optional<Assignment>
+     * @throws ApiException
+     * @throws ReplayException
+     */
+    private Optional<Assignment> chooseAssignment(IProject project, IWorkbenchWindow window) throws 
+        ApiException, ReplayException {
+       
+        Assignment assignment = null;
+        try {
+            Assignment savedAssignment = Activator.getDefault().getProjectManager().getConnection(project);
+            boolean questionResult = MessageDialog.openQuestion(window.getShell(), "Submit",
+                    savedAssignment.getName() + " is connected."
+                            + " Do you want to check with this assignment or change it ?"
+                            + " \n\n Yes = keep \n No = Change assignment");
+            if (questionResult) {
+                assignment = savedAssignment;
+            } else {
+                EclipseLog.info("Showing assignment selector to user");
+                assignment = this.displayAssignmentDialog(window);
+            }
+        } catch (ProjectManagerException ex) {
+            EclipseLog.info("Showing assignment selector to user");
+            assignment = this.displayAssignmentDialog(window);
+        }
+        
+        return Optional.ofNullable(assignment);
     }
 
     /**
